4f3f26be81a3b17f7a7e0454b51d5b09985be9dc
camilapaleno <93452616+TomMilkowski@users.noreply.github.com>
2024-03-27 17:21:44 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
+import { Routes,  Route } from "react-router-dom";
+import Splash from './Splash';
+import Home from './Home';
+import Sal from './projects/Sal';
+import Sal2 from './projects/Sal2';
+import Passion from './projects/Passion';
+import Lantern from './projects/Lantern';
+import Internet from './projects/Internet';
+import Hang from './projects/Hang';
+import Wrist from './projects/Wrist';
+import Guy from './projects/Pastel';
+import Simp from './projects/Simp';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Splash />} />
+      <Route path="/projects" element={<Home />} />
+      <Route path="/sal-fedora" element={<Sal />} />
+      <Route path="/sal-fedora-2" element={<Sal2 />} />
+      <Route path="/passion-policy" element={<Passion />} />
+      <Route path="/lantern" element={<Lantern />} />
+      <Route path="/internet" element={<Internet />} />
+      <Route path="/hang-sesh" element={<Hang />} />
+      <Route path="/wrist-inflation" element={<Wrist />} />
+      <Route path="/simp" element={<Simp />} />
+    </Routes>
   );
 }
 
